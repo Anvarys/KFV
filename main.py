76447d6b24475d64a0bdf795c6309b792818ac95
Anvarys <105349498+Anvarys@users.noreply.github.com>
2024-04-23 18:49:14 +0200

    Some changes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import discord
+import datetime
+import time
 from discord import Intents
 from discord.ext import commands
 from secret import TOKEN
 from role_limits_file import LIMITS
-import datetime
-import time
 from users_file import users
 
 
-CHANNEL_ID = 1209491033474342976 #1073113435660886107
+CHANNEL_ID = 1073113435660886107  # 1209491033474342976
 
 bot = commands.Bot(intents=Intents.default(), command_prefix="!")
 
@@ -25,31 +25,32 @@ async def on_ready():
 
 @bot.tree.command(name="voice_kick", description="Kicks user from voice channel")
 async def voice_kick(interaction: discord.Interaction, user: discord.Member):
+    kicker = interaction.user
     if user.voice is None:
         await interaction.response.send_message(
-            f"You can't kick this user, science they aren't in voice channel.")
+            f"Лох не в войсе KEKWait")
         return
-    if user.id == interaction.user.id:
+    if user.id == kicker.id:
         await interaction.response.send_message(
-            f"You can't your self from voice channel.")
+            f"Ты не можешь кикнуть себя ботяра, иди в школу учись")
         return
     d = datetime.datetime.now(tz=datetime.timezone.utc)
     used_today = 0
-    if user.id in users:
+    if kicker.id in users:
         for time_ in users[user.id]:
             if time_ == str(d.date()):
                 used_today += 1
 
     limit = 0
-    for role in user.roles:
+    for role in kicker.roles:
         if role.id in LIMITS:
             limit = max(limit, LIMITS[role.id])
 
     if used_today >= limit:
-        await interaction.response.send_message(f"You ran out of kicks.\nYou will get your `{limit}` kicks <t:{int(time.time()//1+(23-d.hour)*60*60+(60-d.minute)*60+(60-d.second))}:R>")
+        await interaction.response.send_message(f"У тебя больше нету киков\nХарэ попускать Флакена Возвращайся <t:{int(time.time()//1+(23-d.hour)*60*60+(60-d.minute)*60+(60-d.second))}:R>")
         return
 
-    await interaction.response.send_message(f"You kicked {user.name} from voice channel!\nKicks left: `{limit-used_today-1}`")
+    await interaction.response.send_message(f"Кикнул лоха <@{user.id}> с войса\nЕще можно кикнуть лохов `{limit-used_today-1}` раз")
 
     try:
         await user.move_to(None)
@@ -70,7 +71,7 @@ async def set_role_limit(interaction: discord.Interaction, role: discord.Role, n
     if not interaction.user.guild_permissions.administrator:
         return
     LIMITS[role.id] = new_limit
-    with open("role_limits_file.py","w") as f:
+    with open("role_limits_file.py", "w") as f:
         f.write(f"LIMITS = {LIMITS}")
 
     await interaction.response.send_message(f"Successfully changed kicks limit of role <@&{role.id}> to `{new_limit}`")
@@ -79,7 +80,7 @@ async def set_role_limit(interaction: discord.Interaction, role: discord.Role, n
 @bot.tree.command(name="limits", description="Shows kicks limits for all roles")
 async def limits(interaction: discord.Interaction):
     msg = "Kicks limits for roles:"
-    for role in sorted([(LIMITS[i],i) for i in LIMITS])[::-1]:
+    for role in sorted([(LIMITS[i], i) for i in LIMITS])[::-1]:
         msg += f"\n<@&{role[1]}>: `{role[0]}`"
     await interaction.response.send_message(msg)
 
